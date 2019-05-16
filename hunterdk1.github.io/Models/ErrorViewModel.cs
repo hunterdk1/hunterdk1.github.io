cd8f95218a28e3d5776db93ef4e7eeb46866afee
anders trampedach andersen <anders.trampedach.andersen@gmail.com>
2019-05-16 09:55:38 +0200

    new repositiry

---
+++ hunterdk1.github.io/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace hunterdk1.github.io.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
